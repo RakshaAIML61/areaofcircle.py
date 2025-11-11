23e31ad9e001b28883323fd629950f4142883523
RakshaAIML61 <rakshuu612@gmail.com>
2025-11-11 10:50:39 +0530

    Create ifelseelif.py

---
+++ ifelseelif.py
@@ -0,0 +1,12 @@
+#using if elif else in multicondition
+x=int(input("enter your percentage"))
+if x>=90:
+  print("be happy")
+elif x>=60:
+  print("safe result")
+elif x>=50:
+  print("just pass")
+else:
+  print("u r fail in the exam")
+enter your percentage60
+safe result
