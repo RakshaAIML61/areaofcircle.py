728c6b56d3fda99dce0190040f6bfc75db61c779
RakshaAIML61 <rakshuu612@gmail.com>
2025-11-11 10:52:04 +0530

    Create positivenegative.py

---
+++ positivenegative.py
@@ -0,0 +1,10 @@
+#to check wether a number is posituve negative or zero
+x=float(input("enter the number"))
+if x>0:
+  print("it is a positive number")
+elif x<0:
+  print("it is a negativenumber")
+else:
+  print("it is a zero")
+enter the number-2
+it is a negativenumber
