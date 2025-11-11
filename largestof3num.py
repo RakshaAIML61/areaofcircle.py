c931bf83f5af6687a0737e0cea14d8ebd565a86d
RakshaAIML61 <rakshuu612@gmail.com>
2025-11-11 11:19:40 +0530

    largestof3num.py

---
+++ largestof3num.py
@@ -0,0 +1,17 @@
+#largest of 3 numbers
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+num3=int(input("enter the third number"))
+if (num1>=num2)and (num1>=num2):
+  print("the largest number is",num1)
+elif(num2>=num1) and (num2>=num3):
+  print("the largest nuymber is",num2)
+else:
+  print("the largest number is",num3)
+  
+
+enter the first number23
+enter the second number43
+enter the third number54
+the largest number is 54
+
