55297c46a9214f5102477861454ae9dbf613f98a
RakshaAIML61 <rakshuu612@gmail.com>
2025-11-11 10:53:05 +0530

    Create largestof2num.py

---
+++ largestof2num.py
@@ -0,0 +1,12 @@
+#largest of 2 numbers
+num1=int(input("enter the number"))
+num2=int(input("enter the number"))
+if num1>num2:
+  print("num1 is the largest")
+elif num1<num2:
+  print("num2 is the greatest")
+else:
+  print("both r equal")
+  enter the number16
+enter the number23
+num2 is the greatest
