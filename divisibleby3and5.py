82475487ecea7d911d0e3de9205b69738dfa4c6a
RakshaAIML61 <rakshuu612@gmail.com>
2025-11-11 14:32:41 +0530

    Create divisibleby3and5.py

---
+++ divisibleby3and5.py
@@ -0,0 +1,9 @@
+#divisible by 3 and 5
+a=int(input("enter a num"))
+if a%3==0 and a%5==0:
+  print ("it is divisible by both 3 and 5")
+else:
+  print("it is not divisible by both 3 and 5")
+enter a num15
+it is divisible by both 3 and 5
+
