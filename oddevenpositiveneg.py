6d94e08ef449e67a9bc55bb323b7f7d12b6a2025
RakshaAIML61 <rakshuu612@gmail.com>
2025-11-11 14:50:10 +0530

    Create oddevenpositiveneg.py

---
+++ oddevenpositiveneg.py
@@ -0,0 +1,14 @@
+#even odd and positive negetive positive combine
+num=int(input("enter the numbere"))
+if num%2==0 and num>0:
+  print("even and positive")
+elif num%2!=0 and num<0:
+  print("odd and positive")
+elif num%2==0 and num<0:
+  print("even and negative")
+elif num%2!=2 and num>0:
+  print("odd and negative")
+else:
+  print("the number is 0")
+  enter the numbere13
+odd and negative
