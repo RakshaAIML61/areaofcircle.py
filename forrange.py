76f81fac8ad0b884c40f481bcf08f86465e1c4b7
RakshaAIML61 <rakshuu612@gmail.com>
2025-11-11 12:18:44 +0530

    Create forrange.py

---
+++ forrange.py
@@ -0,0 +1,47 @@
+#grade calculation
+for marks in range(50,90):
+  if marks>=90:
+    print("grade a ")
+  elif marks>=75:
+    print("grade b")
+  elif marks>=50:
+    print("grade c")
+  else:
+    print()
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade c
+grade b
+grade b
+grade b
+grade b
+grade b
+grade b
+grade b
+grade b
+grade b
+grade b
+grade b
+grade b
