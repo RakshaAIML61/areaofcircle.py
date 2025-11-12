fd6e1514563988461a129a119dac4c0963d76b62
RakshaAIML61 <rakshuu612@gmail.com>
2025-11-12 09:50:21 +0530

    Create sumof1stnatural.py

---
+++ sumof1stnatural.py
@@ -0,0 +1,16 @@
+#sym of 1st n natural numbers
+num=int(input("enter a number"))
+if num<0:
+  print("enter a number")
+else:
+  sum=0
+  b=1
+  while b<=1:
+    sum+=b
+    b +=1
+print("the sum is",sum)
+
+
+enter a number12
+the sum is 1
+
