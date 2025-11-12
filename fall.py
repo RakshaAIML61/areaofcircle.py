821335a5469f14a15582e81bfff970578608033d
RakshaAIML61 <rakshuu612@gmail.com>
2025-11-12 15:13:53 +0530

    Create fall.py

---
+++ fall.py
@@ -0,0 +1,14 @@
+def user():
+    name=input("enter ur name")
+    age=int(input("enter ur age"))
+    roll=int(input("enter ur roll"))
+    return name,age,roll, age+roll,age*roll
+user()
+
+
+enter ur namerash
+enter ur age22
+enter ur roll34
+
+('rash', 22, 34, 56, 748)
+
