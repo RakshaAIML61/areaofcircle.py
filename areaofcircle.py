d0981483f980a1f69ce34e2df66a68b816307fe7
RakshaAIML61 <rakshuu612@gmail.com>
2025-11-12 16:08:41 +0530

    Create areaofcircle.py

---
+++ areaofcircle.py
@@ -0,0 +1,12 @@
+#areaofcircle
+def area_of_circle(radius):
+  pi=3.14
+  return pi*radius*radius
+radius=int(input("enter_the_radius"))
+print("area of circle")
+
+
+enter_the_radius2
+area of circle
+
+[ ]
