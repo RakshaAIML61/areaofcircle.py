f390c371037cf1bfe51f4417f2b91f5ceb070f23
RakshaAIML61 <rakshuu612@gmail.com>
2025-11-12 12:20:14 +0530

    Create fibbinousi.py

---
+++ fibbinousi.py
@@ -0,0 +1,30 @@
+num=int(input("enter a number"))
+a , b= 0,1
+for i in range(1, num+1):
+  print(a,)
+  a,b=b,a+b
+  enter a number24
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144
+233
+377
+610
+987
+1597
+2584
+4181
+6765
+10946
+17711
+28657
