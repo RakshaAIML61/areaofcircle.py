52648f55979fb6dab30e690561cdbf00d2533ed1
RakshaAIML61 <rakshuu612@gmail.com>
2025-11-14 15:22:44 +0530

    Create list5.py

---
+++ list5.py
@@ -0,0 +1,17 @@
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+c=int(input("enter a number"))
+d=int(input("enter a number"))
+e=int(input("enter a number"))
+t=[a,b,c,d,e]
+print(t)
+
+
+enter a number23
+enter a number4
+enter a number4
+enter a number5
+enter a number66
+[23, 4, 4, 5, 66]
+
+
