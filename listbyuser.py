7c9460c4361dd16b0ada2b9e68c1b93bd8682ba2
RakshaAIML61 <rakshuu612@gmail.com>
2025-11-14 11:26:48 +0530

    Create listbyuser.py

---
+++ listbyuser.py
@@ -0,0 +1,16 @@
+a=int(input("enter a num"))
+b=int(input("enter a num"))
+c=int(input("enter a num"))
+d=int(input("enter a num"))
+e=int(input("enter a num"))
+l=[a, b, c ,d ,e]
+print(l)
+
+
+enter a num44
+enter a num44
+enter a num78
+enter a num36
+enter a num49
+[44, 44, 78, 36, 49]
+
